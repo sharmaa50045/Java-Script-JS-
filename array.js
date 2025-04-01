ca0aface8299ca70dcc636183c1f6f7af508af0f
Aaditya <sharmaa50045@gmail.com>
2025-04-01 20:25:59 +0530

    update array

--- array.js
+++ array.js
@@ -1,5 +1,24 @@
-let cities=["Delhi","Mumbai","Bihar"];
-let arr =cities.at(1);
-let arrn =cities.at(-3);
-console.log(arr);
-console.log(arrn);
+// let cities=["Delhi","Mumbai","Bihar"];
+// let arr =cities.at(1);
+// let arrn =cities.at(-3);
+// console.log(arr);
+// console.log(arrn);
+
+// let fruits =["apple","banana","mango","orange"];
+// let result = fruits.at(1);
+// console.log(result);
+
+// let fruits =["apple","banana","mango","orange"];
+// let result = fruits.at(-1);
+// console.log(result);
+
+// let numbers =[10,20,30];
+// console.log(numbers.at(5))
+// console.log(numbers.at(-4))
+
+let cities=["delhi","mumbai","chennai"];
+
+function getLastElement(arr){
+    return arr.at(-1);
+}
+    console.log(getLastElement(cities));
