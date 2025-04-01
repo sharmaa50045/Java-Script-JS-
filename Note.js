b67d4af339b764571491b27e65dcfb1064efebde
Aaditya <sharmaa50045@gmail.com>
2025-04-01 19:12:05 +0530

    update array

---
+++ Note.js
@@ -0,0 +1,10 @@
+
+// var val1 = 10;
+// var val2 = 15;
+// console.log(val1+val2)
+
+var val0 = 10;
+var val1 = 15;
+val2 = 200
+var val3 = 300;
+console.log (val0+val1+val2+val3)
---
+++ array.js
@@ -0,0 +1,5 @@
+let cities=["Delhi","Mumbai","Bihar"];
+let arr =cities.at(1);
+let arrn =cities.at(-3);
+console.log(arr);
+console.log(arrn);
